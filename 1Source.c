ef7b485f1781bcb8720cade245634d3ebcd709bf
Eldar-Saltykov <112901922+Eldar-Saltykov@users.noreply.github.com>
2022-10-13 01:41:31 +0300

    Add files via upload

---
+++ 1Source.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#include<locale.h>
+#define _USE_MATH_DEFINES
+#define M_PI 3.14159265358979323846
+
+void main()
+{
+    int gr;
+    float gr1;
+    setlocale(LC_ALL, "RUS");
+    puts("¬ведите градусы");
+    scanf(" %d", &gr);
+    gr1 = sin(gr * M_PI / 180);
+    printf("Ёто равно %.6f родиан", gr1);
+}
